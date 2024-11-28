fc016b11651b07cf9d5a92f6e96524c1da901f02
kendysxsiden <kirillvrublevskiy@gmail.com>
2024-11-28 10:31:25 +0100

    up

--- calc.js
+++ calc.js
@@ -1,18 +1,16 @@
-// Добавить значение в поле ввода
+
 function appendValue(value) {
     document.getElementById("result").value += value;
 }
 
-// Очистить поле ввода
 function clearResult() {
     document.getElementById("result").value = "";
 }
 
-// Выполнить расчет
 function calculate() {
     try {
         const input = document.getElementById("result").value;
-        const result = eval(input); // Выполняем расчет
+        const result = eval(input); 
         document.getElementById("result").value = result;
     } catch (error) {
         alert("Invalid expression!");
--- cals.js
+++ cals.js
@@ -1,15 +1,15 @@
 function appendValue(value) {
     document.getElementById("result").value += value;
 }
-// Очистить поле ввода
+
 function clearResult() {
     document.getElementById("result").value = "";
 }
-// Выполнить расчет
+
 function calculate() {
     try {
         const input = document.getElementById("result").value;
-        const result = eval(input); // Выполняем расчет
+        const result = eval(input); 
         document.getElementById("result").value = result;
     } catch (error) {
         alert("Invalid expression!");
